da6f32a4a154f682bcb84ee8d8df190037dfbfe0
ef718 <elfang16@gmail.com>
2014-07-02 16:36:12 -0400

    Updated chart type

--- js/main.js
+++ js/main.js
@@ -1,230 +1,130 @@
-var canvasW = 800;
-var canvasH = 300;
+var Canvas = require('canvas'),
+    fs = require('fs');
 
-// Create the SVG image...
+var world = JSON.parse(fs.readFileSync('110m_admin_0_countries.json','utf8'));
+var colorRegions = fs.readFileSync('../data/color_regions.csv','utf8');
 
-var svg = d3.select('#graph')
-  .append('svg')
-  .attr('width', canvasW)
-  .attr('height', canvasH)
+var w = 125,
+    h =  63;
 
-
-function redrawGraph() {
-
-  // Redraw the graph image...
-  var data = getRandomDataset();
-
-  var scaleY = d3.scale.linear()
-    .range([0, canvasH])
-    .domain([0, d3.max(data)]);
-
-  var bars = svg.selectAll('rect').data(data);
-
-  bars
-    .enter()
-    .append('rect')
-    .attr('y', canvasH)
-    .attr('height', 0);
-
-  // "d" and "i" are "data" and "index"
-  bars
-    .transition()
-    .attr('x', function(d, i) { return i* (canvasW / data.length) })
-    .attr('y', function(d) { return canvasH - scaleY(d); })
-    .attr('width', 30)
-    .attr('height', function(d) { return scaleY(d); })
-    .attr('fill', function(d) { return 'rgb(0,0,'+ d*10 +')'; });
-
-  bars
-    .exit()
-    .transition()
-    .attr('height', 0)
-    .remove();
+var project = {
+  'Point': function(coords) {
+    coords[0] = w * (coords[0] + 180.0) / 360.0;
+    coords[1] = h - (h * (coords[1] + 90.0) / 180.0);
+  },
+ 'LineString': function(coords) {
+    coords.forEach(project.Point);
+  },
+ 'Polygon': function(coords) {
+    coords.forEach(project.LineString);
+  },
+ 'MultiPolygon': function(coords) {
+    coords.forEach(project.Polygon);
+  },
+ 'Feature': function(f) {
+    project[f.geometry.type](f.geometry.coordinates);
+  }
 }
 
-// Various accessors that specify the four dimensions of data to visualize.
-function x(d) { return d.income; }
-function y(d) { return d.lifeExpectancy; }
-function radius(d) { return d.population; }
-function color(d) { return d.region; }
-function key(d) { return d.name; }
+world.features.forEach(project.Feature);
 
-// Chart dimensions.
-var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5},
-    width = 960 - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var regionsByCountryCode = {},
+    regionCountryById = {},
+    uniqueRegions = {},
+    regions = [],
+    colors = [ '#3F4FFF', '#2FBFE5', '#68FF5E', '#E5FF2F', '#FF982F', '#FF2F2F' ];
 
-// Various scales. These domains make assumptions of data, naturally.
-var xScale = d3.scale.log().domain([300, 1e5]).range([0, width]),
-    yScale = d3.scale.linear().domain([10, 85]).range([height, 0]),
-    radiusScale = d3.scale.sqrt().domain([0, 5e8]).range([0, 40]),
-    colorScale = d3.scale.category10();
+var lines = colorRegions.split('\n'),
+    rows = lines.map(function(line) {
+      var words = line.split(',');
+      var columns = [];
+      while(words.length > 0) {
+        var word = words.shift();
+        if (word.charAt(0) == '"') {
+          while(word.charAt(word.length-1) != '"' && words.length > 0) {
+            word += ','+words.shift();
+          }
+        }
+        if (word.charAt(0) == '"' && word.charAt(word.length-1) == '"') {
+          word = word.slice(1,word.length-1);
+        }
+        columns.push(word);
+      }
+      return columns;
+    }),
+    headers = rows.shift();
 
-// The x & y axes.
-var xAxis = d3.svg.axis().orient("bottom").scale(xScale).ticks(12, d3.format(",d")),
-    yAxis = d3.svg.axis().scale(yScale).orient("left");
-
-// Create the SVG container and set the origin.
-var svg = d3.select("#chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// Add the x-axis.
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-
-// Add the y-axis.
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
-
-// Add an x-axis label.
-svg.append("text")
-    .attr("class", "x label")
-    .attr("text-anchor", "end")
-    .attr("x", width)
-    .attr("y", height - 6)
-    .text("income per capita, inflation-adjusted (dollars)");
-
-// Add a y-axis label.
-svg.append("text")
-    .attr("class", "y label")
-    .attr("text-anchor", "end")
-    .attr("y", 6)
-    .attr("dy", ".75em")
-    .attr("transform", "rotate(-90)")
-    .text("life expectancy (years)");
-
-// Add the year label; the value is set on transition.
-var label = svg.append("text")
-    .attr("class", "year label")
-    .attr("text-anchor", "end")
-    .attr("y", height - 24)
-    .attr("x", width)
-    .text(1800);
-
-// Load the data.
-d3.json("nations.json", function(nations) {
-
-  // A bisector since many nation's data is sparsely-defined.
-  var bisect = d3.bisector(function(d) { return d[0]; });
-
-  // Add a dot per nation. Initialize the data at 1800, and set the colors.
-  var dot = svg.append("g")
-      .attr("class", "dots")
-    .selectAll(".dot")
-      .data(interpolateData(1800))
-    .enter().append("circle")
-      .attr("class", "dot")
-      .style("fill", function(d) { return colorScale(color(d)); })
-      .call(position)
-      .sort(order);
-
-  // Add a title.
-  dot.append("title")
-      .text(function(d) { return d.name; });
-
-  // Add an overlay for the year label.
-  var box = label.node().getBBox();
-
-  var overlay = svg.append("rect")
-        .attr("class", "overlay")
-        .attr("x", box.x)
-        .attr("y", box.y)
-        .attr("width", box.width)
-        .attr("height", box.height)
-        .on("mouseover", enableInteraction);
-
-  // Start a transition that interpolates the data based on year.
-  svg.transition()
-      .duration(30000)
-      .ease("linear")
-      .tween("year", tweenYear)
-      .each("end", enableInteraction);
-
-  // Positions the dots based on data.
-  function position(dot) {
-    dot .attr("cx", function(d) { return xScale(x(d)); })
-        .attr("cy", function(d) { return yScale(y(d)); })
-        .attr("r", function(d) { return radiusScale(radius(d)); });
+rows.forEach(function(row) {
+  var group = row[1],
+      name = row[0],
+      id = row[2];
+  if (!(group in uniqueRegions)) {
+    uniqueRegions[group] = true;
+    regions.push(group);
   }
-
-  // Defines a sort order so that the smallest dots are drawn on top.
-  function order(a, b) {
-    return radius(b) - radius(a);
-  }
-
-  // After the transition finishes, you can mouseover to change the year.
-  function enableInteraction() {
-    var yearScale = d3.scale.linear()
-        .domain([1800, 2009])
-        .range([box.x + 10, box.x + box.width - 10])
-        .clamp(true);
-
-    // Cancel the current transition, if any.
-    svg.transition().duration(0);
-
-    overlay
-        .on("mouseover", mouseover)
-        .on("mouseout", mouseout)
-        .on("mousemove", mousemove)
-        .on("touchmove", mousemove);
-
-    function mouseover() {
-      label.classed("active", true);
-    }
-
-    function mouseout() {
-      label.classed("active", false);
-    }
-
-    function mousemove() {
-      displayYear(yearScale.invert(d3.mouse(this)[0]));
-    }
-  }
-
-  // Tweens the entire chart by first tweening the year, and then the data.
-  // For the interpolated data, the dots and label are redrawn.
-  function tweenYear() {
-    var year = d3.interpolateNumber(1800, 2009);
-    return function(t) { displayYear(year(t)); };
-  }
-
-  // Updates the display to show the specified year.
-  function displayYear(year) {
-    dot.data(interpolateData(year), key).call(position).sort(order);
-    label.text(Math.round(year));
-  }
-
-  // Interpolates the dataset for the given (fractional) year.
-  function interpolateData(year) {
-    return nations.map(function(d) {
-      return {
-        name: d.name,
-        region: d.region,
-        income: interpolateValues(d.income, year),
-        population: interpolateValues(d.population, year),
-        lifeExpectancy: interpolateValues(d.lifeExpectancy, year)
-      };
-    });
-  }
-
-  // Finds (and possibly interpolates) the value for the specified year.
-  function interpolateValues(values, year) {
-    var i = bisect.left(values, year, 0, values.length - 1),
-        a = values[i];
-    if (i > 0) {
-      var b = values[i - 1],
-          t = (year - a[0]) / (b[0] - a[0]);
-      return a[1] * (1 - t) + b[1] * t;
-    }
-    return a[1];
-  }
+  regionsByCountryCode[id] = group;
+  regionCountryById[id] = name;
 });
 
-console.log("testing! Gas Prices!");
+var geomByCountryCode = {};
 
+world.features.forEach(function(f) {
+  geomByCountryCode[f.properties.ISO_A2] = f.geometry;
+  geomByCountryCode[f.properties.ADMIN] = f.geometry;
+});
+
+geomByCountryCode['SD'].coordinates = [ geomByCountryCode['Sudan'].coordinates, geomByCountryCode['South Sudan'].coordinates ];
+geomByCountryCode['SD'].type = 'MultiPolygon';
+
+var canvas = new Canvas(),
+    ctx = canvas.getContext('2d');
+
+canvas.width = w;
+canvas.height = h;
+
+ctx.antialias = 'none';
+
+for (var id in regionsByCountryCode) {
+  var geom = geomByCountryCode[id] || geomByCountryCode[regionCountryById[id]];
+  ctx.fillStyle = colors[regions.indexOf(regionsByCountryCode[id])];
+  if (geom) {
+    switch(geom.type) {
+      case 'Polygon':
+        ctx.beginPath();
+        geom.coordinates.forEach(function(ring) {
+          ctx.moveTo(ring[0][0], ring[0][1]);
+          ring.slice(1).forEach(function(point) {
+            ctx.lineTo(point[0], point[1]);
+          });
+        });
+        ctx.fill();
+        break;
+      case 'MultiPolygon':
+        ctx.beginPath();
+        geom.coordinates.forEach(function(polygon) {
+          polygon.forEach(function(ring) {
+            ctx.moveTo(ring[0][0], ring[0][1]);
+            ring.slice(1).forEach(function(point) {
+              ctx.lineTo(point[0], point[1]);
+            });
+          });
+        });
+        ctx.fill();
+        break;
+    }
+  }
+  else {
+    console.error('no geom for %s', id);
+  }
+}
+
+var out = fs.createWriteStream(__dirname + '/world.png'),
+    stream = canvas.createPNGStream();
+
+stream.on('data', function(chunk){
+  out.write(chunk);
+});
+
+stream.on('end', function(){
+  console.log('saved png');
+});
