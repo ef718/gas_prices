3233765bab1cd0fa3321e6c0a248bc23f3048756
jeva33 <jonathaneva@gmail.com>
2014-07-03 17:11:01 -0400

    work in progress :)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-/*
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -23,16 +22,10 @@ var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.price); });
 
-var area = d3.svg.area()
-    .x(function(d) { return x(d.date); })
-    .y0(height)
-    .y1(function(d) { return y(d.price); });
-
 var svg = d3.select("body").append("svg")
-    .classed({ 'graph-background': true })
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+  .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
@@ -70,7 +63,6 @@ d3.json("http://api.eia.gov/series/?api_key=5BDF29D557600AA90937165DE3D697D7&ser
       .attr("class", "y axis")
       .call(yAxis)
     .append("text")
-      .classed({ 'graph-text': true })
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
       .attr("dy", ".71em")
@@ -80,86 +72,9 @@ d3.json("http://api.eia.gov/series/?api_key=5BDF29D557600AA90937165DE3D697D7&ser
   svg.selectAll("path").remove();
 
   svg.append("path")
-    .datum(gasPrices)
-    .attr("class", "area")
-    .attr("d", area);
-
-
-  // svg.append("path")
-  //   .datum(gasPrices)
-  //   .attr("class", "area")
-  //   .attr("d", area);
-
-});
-*/
-
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-var parseDate = d3.time.format("%Y%m%d").parse;
-
-var x = d3.time.scale()
-    .range([0, width]);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var area = d3.svg.area()
-    .x(function(d) { return x(d.date); })
-    .y0(height)
-    .y1(function(d) { return y(d.price); });
-
-var svg = d3.select("body").append("svg")
-    .classed({ 'graph-background': true })
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.json("http://api.eia.gov/series/?api_key=5BDF29D557600AA90937165DE3D697D7&series_id=PET.EMM_EPMRU_PTE_NUS_DPG.W", function(error, rawData) {
-  rawData = rawData["series"]["0"]["data"];
-  var data = [];
-
-  for (var i = 0; i < rawData.length; i++) {
-    var num = rawData[i][1];
-
-    data.push({
-      date: parseDate(rawData[i][0]),
-      price: isNaN(num) ? 0 : num
-    });
-  }
-
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain([0, d3.max(data, function(d) { return d.price; })]);
-
-  svg.append("path")
-      .datum(data)
-      .attr("class", "area")
-      .attr("d", area);
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Price ($)");
+      .datum(gasPrices)
+      .attr("class", "line")
+      .attr("d", line);
 });
 
 $(function(){
